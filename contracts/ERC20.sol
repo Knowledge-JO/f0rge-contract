f36d1609bb89f948a9c388c56e866e545a041d86
yomidep <noabiliaminfirst@gmail.com>
2023-12-22 15:51:43 +0100

    compiling works now

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/ERC20.sol)
 
-pragma solidity >=0.8.0 <=0.8.20;
+pragma solidity 0.8.20;
 
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC20Metadata} from "@openzeppelin/contracts/extensions/IERC20Metadata.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 import {IERC20Errors} from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
